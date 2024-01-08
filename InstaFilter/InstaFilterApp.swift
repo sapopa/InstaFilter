cb5c88b50906f2955db19cb042a06ea823fcc20d
sebastian.popa <sebastian.popa@softvision.com>
2024-01-08 07:09:36 +0200

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by sebastian.popa on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
